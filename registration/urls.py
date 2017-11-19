3fd55e7f63ebf8894e0bf1f6ebb1f5d7ef7c68fe
tomng2012 <tomng2012@gmail.com>
2017-11-19 18:07:18 -0500

    Completed navigation layout

--- registration/urls.py
+++ registration/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/registration/login'}, name='logout'),
     url(r'^home/$', views.home, name='home'),
     url(r'^register/$', views.register, name='register'),
     url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
