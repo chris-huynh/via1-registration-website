ead8a5a39a78b88a1acec88a530480334d63d447
Thomas Nguyen <nguye2t5@mail.uc.edu>
2017-11-20 20:37:15 -0500

    Added timezone to landing page datetimes and fixed createsuperuser in managers.py

--- registration/managers.py
+++ registration/managers.py
@@ -26,7 +26,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_active', False)
         return self._create_user(email, password, first_name, last_name, **extra_fields)
 
-    def create_superuser(self, email, password, first_name, last_name, **extra_fields):
+    def create_superuser(self, email, password, first_name=None, last_name=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
