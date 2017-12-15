8efcf8dccd286d3e739d4a19b43f1b0d759cb206
tomng2012 <tomng2012@gmail.com>
2017-12-14 20:12:26 -0500

    Adding filters for User admin

--- registration/admin.py
+++ registration/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 
 from registration.models import ConferenceVars, Workshops, User, UserInfo, Families
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ['has_paid', 'has_paid_hotel']
+
+
 # Register your models here.
 admin.site.register(ConferenceVars)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(UserInfo)
 admin.site.register(Workshops)
 admin.site.register(Families)
