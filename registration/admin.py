d9340524a57aed68c6c8bbcd3cf38e9b48e40dfb
Thomas Nguyen <nguye2t5@mail.uc.edu>
2018-01-21 18:35:38 -0500

    Added additional filtering and search field for admin page

--- registration/admin.py
+++ registration/admin.py
@@ -7,13 +7,18 @@ admin.site.site_header = 'VIA-1 Administration'
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_filter = ['has_paid', 'has_paid_hotel']
+    list_filter = ['has_paid', 'has_paid_hotel', 'is_staff', 'hotel_type']
+    search_fields = ['first_name', 'last_name', 'email']
+
+
+class UserInfoAdmin(admin.ModelAdmin):
+    list_filter = ['school', 'family', 'banquet_meal', 'shirt_size', 'pronouns']
 
 
 # Register your models here.
 admin.site.register(ConferenceVars)
 admin.site.register(User, UserAdmin)
-admin.site.register(UserInfo)
+admin.site.register(UserInfo, UserInfoAdmin)
 admin.site.register(Workshops)
 admin.site.register(Families)
 admin.site.register(SpecialRegCodes)
